312b2a38d0f831ce5a119457fc4081cf5699c0dd
Javier210972 <37030402+Javier210972@users.noreply.github.com>
2018-03-04 19:36:43 -0600

    Add files via upload

---
+++ ejerciciociclos/Solucion2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciociclos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author redes
+ */
+public class Solucion2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Solicitar dos valores numéricos enteros, calcular el promedio y mostrar de
+forma inversa los números (del mayor al menor) desde el promedio hasta el
+número 1.*/
+
+        Scanner Inverso = new Scanner(System.in);
+        int numero;
+        int numeroDos;
+        double Pro;
+
+        System.out.println("Ingrese dos numeros");
+        numero = Inverso.nextInt();
+        numeroDos = Inverso.nextInt();
+        Pro = (double) (numero + numeroDos) / 2;
+
+        double i;
+
+        System.out.println("");
+        System.out.println("Promedio: " + Pro);
+        for (i = Pro; i >= 1; i--) {
+
+            System.out.println(i);
+
+        }
+       
+
+        }
+
+    }
+
